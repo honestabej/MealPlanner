7c8b16c4aef082c5943ab1d7ae94918c528b85d3
honestabej <honestabej@hotmail.com>
2025-02-07 09:11:26 -0700

    Initial Commit

---
+++ MealPlanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealPlanner
+//
+//  Created by Abe Johnson on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MealPlannerTests/MealPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerTests.swift
+//  MealPlannerTests
+//
+//  Created by Abe Johnson on 2/7/25.
+//
+
+import Testing
+@testable import MealPlanner
+
+struct MealPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
